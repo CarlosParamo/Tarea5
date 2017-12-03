a64d46afec7f4ce5548d01a74be57dbfab66b048
CarlosParamo <capr24398@gmail.com>
2017-12-03 04:29:23 -0500

    Avance punteros y funciones

--- CurvaRotacion.c
+++ CurvaRotacion.c
@@ -2,71 +2,91 @@
 #include <stdlib.h>
 #include <math.h>
 
-
 int N=300;
-float bb=0.2497;
-float bd=5.16;
-float ad=0.3105;
-float ah=64.3;
+double bb=0.2497;
+double bd=5.16;
+double ad=0.3105;
+double ah=64.3;
+
+void loadata(double *x, double *y);
+void modelo(double *v, double *R, double *Mb, double *Md, double *Mh);
+void likelihood(double *yobs, double *ymod, double a);
 
 float main(void){
+
+        double *Mb_walk, *Md_walk, *Mh_walk, *vdata, *rdata, *vmodel;
        
 	Mb_walk=malloc(N*sizeof(double));
 	Md_walk=malloc(N*sizeof(double));
-        Mh_walk=malloc(N*sizeof(double)); 
+        Mh_walk=malloc(N*sizeof(double));
+
+        Mb_walk[0]=10.0*drand48();
+        Md_walk[0]=10.0*drand48();
+	Mh_walk[0]=10.0*drand48();
+      
+        /*y_init = my_model(x_obs, m_walk[0], b_walk[0])*/
+        
 
         vdata=malloc(N*sizeof(double));
         rdata=malloc(N*sizeof(double));
         loadata(rdata,vdata);
-
+        int i;
+	for(i=0;i<N;i++)
+	{
+                printf("%lf ",rdata[i]);
+                printf("%lf\n",vdata[i]);
+	}
+        
+	printf("hasta aqui funciona\n");
 
         vmodel=malloc(N*sizeof(double));
-	modelo(vmodel,rdata);
-
-}
-
-void modelo(double *v, double R*)
-{
-         for(i=1;i<(n_puntos-1);i++)
-         {
-		v[i]=(sqrt(Mb)*R[i])/(pow(R[i]*R[i]+bb*bb,3.0/4.0))+(sqrt(Md)*R[i])/(pow(R[i]*R[i]+(bd+ad)*(bd+ad),3.0/4.0))+(sqrt(Mh))/(pow(R[i]*R[i]+ah*ah,1.0/4.0))
-	}
-	
-}
-
-
-void likelihood(double *yobs, double *ymod)
-{
+        int chicua;
         
+	modelo(vmodel,rdata,Mb_walk, Md_walk, Mh_walk);
+        likelihood(vdata,vmodel,chicua);
 
-        for(i=1;i<(n_puntos-1);i++)
+}
+
+void modelo(double *v, double *R, double *Mb, double *Md, double *Mh)
+{
+         int i = 0;
+         for(i=1;i<(N-1);i++)
+         {
+		v[i]= (sqrt(Mb[i])*R[i])/(pow(R[i]*R[i]+bb*bb,3.0/4.0)) + (sqrt(Md[i])*R[i])/(pow(R[i]*R[i]+(bd+ad)*(bd+ad),3.0/4.0)) + (sqrt(Mh[i]))/(pow(R[i]*R[i]+ah*ah,1.0/4.0));
+	 }
+}
+
+void likelihood(double *yobs, double *ymod, double a)
+{
+        int suma=0;
+	int i = 0;
+        double chisq=0;
+        for(i=1;i<(N-1);i++)
         {
-
-           
+        	suma+=(yobs[i]-ymod[i])*(yobs[i]-ymod[i]);
 	}
 
-	chisq = (1.0/2.0)*suma
-
-
+	chisq = (1.0/2.0)*suma;
+        a= exp(-chisq);
+        
 }
 
 void loadata(double *x, double *y)
 {
-    int i = 0;
-    double number;
-    FILE *file = fopen("RadialVelocities.dat", "r");
-
-    while(fscanf(file, "%lf\n", &number)==1)
-    {
+    	int i = 0;
+	double number;
+	FILE *file = fopen("RadialVelocities.dat", "r");
+	while(fscanf(file, "%lf\n", &number)==1)
+     	{
         
-        if(i%2==0 and i<0)
-        {
-            x[i/2] = number;
-        }
-        else
-        {
-            y[i/2] = number;
-        }
-        i += 1;
-    }
+        	if(i%2==0)
+        	{
+        	    x[i/2] = number;
+        	}
+        	else
+        	{
+        	    y[i/2] = number;
+        	}
+        	i += 1;
+    	}	
 }
