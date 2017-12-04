9027146dfae3f6cb62d086e010446b09f9aaccdb
CarlosParamo <capr24398@gmail.com>
2017-12-03 22:48:58 -0500

    Plot

--- Plots.py
+++ Plots.py
@@ -1,7 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-a = np.genfromtxt("RadialVelocities.dat", delimiter=None, skip_header=0)
+a= np.genfromtxt("RadialVelocities.dat", delimiter=None, skip_header=0)
+#b= np.genfromtxt("dataout.dat", delimiter=None)
 
-plt.plot(a[:,0],a[:,1])
+plt.plot(a[:,0],a[:,1], label='Datos')
+#plt.plot(b[:,0],b[:,1], label='Modelo')
+plt.legend(loc = 1) 
+plt.ylabel("$Velocidad[km/s]$")
+plt.xlabel("$Radio[Kpc]$")
 plt.show() 
